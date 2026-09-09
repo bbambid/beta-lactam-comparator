70a4b6843ee060aeb0954d31aaa7431e56e32e09
bbambid <gateasus0718@ezweb.ne.jp>
2026-09-09 15:55:29 +0900

    Complete pediatric categories and stabilize category tabs

--- pediatric-dose-audit/category-picker-loader-2026-09.js
+++
@@ -1,4 +0,0 @@
-(function(){
- const css=document.createElement('link');css.rel='stylesheet';css.href='category-layout-fixes-2026-09.css';document.head.appendChild(css);
- const s=document.createElement('script');s.src='category-picker-final-2026-09.js';document.body.appendChild(s);
-})();
