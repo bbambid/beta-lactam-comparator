5873b7bfa145fc0d97e8bdbe107bdf342a41d6c7
bbambid <gateasus0718@ezweb.ne.jp>
2026-09-09 15:54:04 +0900

    Load category picker fixes

---
+++ pediatric-dose-audit/category-picker-loader-2026-09.js
@@ -0,0 +1,4 @@
+(function(){
+ const css=document.createElement('link');css.rel='stylesheet';css.href='category-layout-fixes-2026-09.css';document.head.appendChild(css);
+ const s=document.createElement('script');s.src='category-picker-final-2026-09.js';document.body.appendChild(s);
+})();
